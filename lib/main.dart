c4072bb8e2b4c749e6bbc385496133ba1faaa9cd
Mohamed DIABY <mdiaby@spark.local>
2020-09-14 08:45:12 +0100

    making the list scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyHomePage extends StatelessWidget{
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: UserTransaction(),
+      body: SingleChildScrollView(child: UserTransaction()),
       );
   }
 }
