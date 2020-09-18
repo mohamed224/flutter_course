027a06da25d1425c75450507bf8634ef960f560b
Mohamed DIABY <mdiaby@spark.local>
2020-09-18 14:25:14 +0100

    improving & styling the modal buttom sheet

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submittedData(){
     final enteredTitle = titleController.text;
@@ -14,8 +21,10 @@ class NewTransaction extends StatelessWidget {
     if(enteredTitle.isEmpty || enteredAmount<=0){
       return;
     }
-    addTx(enteredTitle, enteredAmount);
+    widget.addTx(enteredTitle, enteredAmount);
+    Navigator.of(context).pop();
   }
+
   @override
   Widget build(BuildContext context) {
     return Card(
