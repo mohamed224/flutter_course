eb861d41f12c6db6acfffaa0a336c0ef10ad1ebf
Mohamed DIABY <mdiaby@spark.local>
2020-09-18 11:38:28 +0100

    Further Input & Output Styling and Configuration

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -8,6 +8,15 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submittedData(){
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if(enteredTitle.isEmpty || enteredAmount<=0){
+      return;
+    }
+    addTx(
+        enteredTitle, enteredAmount);
+  }
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,18 +29,18 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_)=> submittedData(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_)=> submittedData(),
             ),
             FlatButton(
               child: Text('Add Transaction'),
               textColor: Colors.purple,
-              onPressed: () {
-                addTx(
-                    titleController.text, double.parse(amountController.text));
-              },
+              onPressed:submittedData,
             )
           ],
         ),
