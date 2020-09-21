87029cfac278b226ec040ce737d1a0dfe07607b7
Mohamed DIABY <mdiaby@spark.local>
2020-09-21 16:34:42 +0100

    deleting transactions & using iconButtons

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.purple,
           accentColor: Colors.amber,
+          errorColor: Colors.red,
           fontFamily: 'Quicksand',
           textTheme: ThemeData.light().textTheme.copyWith(
               title: TextStyle(
@@ -80,6 +81,12 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
+  void  _deleteTransaction(String id){
+    setState(() {
+      _userTransactions.removeWhere((tx) =>tx.id == id);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -99,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Chart(_recentTransactions),
-          TransactionList(_userTransactions),
+          TransactionList(_userTransactions , _deleteTransaction),
         ],
       )),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
