1ba5900cf7502bb64866d1b2bb97238d98901163
Mohamed DIABY <mdiaby@spark.local>
2020-09-21 14:22:38 +0100

    adding a listTile widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -31,40 +31,26 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.purple, width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                              color: Colors.purple),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          Text(
-                            DateFormat.yMMMMd()
-                                .format(transactions[index].date),
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
+               return Card(
+                 elevation: 5,
+                 margin: EdgeInsets.symmetric(vertical: 8 , horizontal: 5),
+                 child: ListTile(
+                   leading: CircleAvatar(
+                     radius: 30,
+                     child: Padding(
+                       padding: const EdgeInsets.all(6),
+                       child: FittedBox(
+                           child: Text( '\$${transactions[index].amount}')
+                       ),
+                     )  ,
+                   ),
+                   title: Text(
+                     transactions[index].title,
+                     style: Theme.of(context).textTheme.title,),
+                   subtitle: Text(
+                     DateFormat.yMMMd().format((transactions[index].date))),
+                 ),
+               );
               },
               itemCount: transactions.length),
     );
